15341744aa80c06bc6c721d5c9da943805ce8976
Mustafa.Saglam <Mustafa.Saglam@KSN70002179>
2018-11-27 14:18:35 +0300

    Add project files.

---
+++ DataTable/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        // GET: Home
+        
+        public ActionResult Index()
+        {
+
+            return View(db.Products.ToList());
+        }
+    }
+}
